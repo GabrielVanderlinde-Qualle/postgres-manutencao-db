93b31f0429d18b51b6e9caa7bdd23ed81079d74e
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-30 13:55:13 -0300

    config: ativa carregamento automatico de entidades e corrige update

--- manutencao.nest/src/app.module.ts
+++ manutencao.nest/src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ManutencaoModule } from './manutencao/manutencao.module';
+import { TipoSistemaModule } from './tipo_sistema/tipo_sistema.module';
+import { TipoOperacaoModule } from './tipo_operacao/tipo_operacao.module';
+import { TipoCriticidadeModule } from './tipo_criticidade/tipo_criticidade.module';
 
 @Module({
   imports: [
@@ -14,9 +17,13 @@ import { ManutencaoModule } from './manutencao/manutencao.module';
       password: 'postgre123',
       database: 'manutencao_db',
       entities: [],
+      autoLoadEntities: true,
       synchronize: false, //Tabelas já Criadas no Banco Local
     }),
     ManutencaoModule,
+    TipoSistemaModule,
+    TipoOperacaoModule,
+    TipoCriticidadeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- manutencao.nest/src/manutencao/manutencao.controller.ts
+++ manutencao.nest/src/manutencao/manutencao.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ManutencaoService } from './manutencao.service';
 import { CreateManutencaoDto } from './dto/create-manutencao.dto';
-import { UpdateManutencaoDto } from './dto/update-manutencao.dto';
 
 @Controller('manutencao')
 export class ManutencaoController {
   constructor(private readonly manutencaoService: ManutencaoService) {}
 
+  // 1. Criar manutenção
   @Post()
   create(@Body() createManutencaoDto: CreateManutencaoDto) {
     return this.manutencaoService.create(createManutencaoDto);
   }
 
+  // URL: http://localhost:3000/manutencao/relatorio-geral
+  @Get('relatorio-geral')
+  getRelatorio() {
+    return this.manutencaoService.gerarRelatorio();
+  }
+
+  // URL: http://localhost:3000/manutencao/dashboard
+  @Get('dashboard')
+  getDashboard() {
+    return this.manutencaoService.contarPorSistema();
+  }
+
+  // 2. Listar todas (padrão)
   @Get()
   findAll() {
     return this.manutencaoService.findAll();
   }
 
+  //3. Buscar uma manutenção pelo ID
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.manutencaoService.findOne(+id);
+    return `Esta ação retorna a manutenção #${id}`;
   }
 
+  // Atualzar Manutenção
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateManutencaoDto: UpdateManutencaoDto) {
-    return this.manutencaoService.update(+id, updateManutencaoDto);
+  update(@Param('id') id: string) {
+    return `Esta ação atualiza a manutenção #${id}`;
   }
 
+  // Excluir Manutenção
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.manutencaoService.remove(+id);
+    return `Esta ação remove a manutenção #${id}`;
   }
 }
