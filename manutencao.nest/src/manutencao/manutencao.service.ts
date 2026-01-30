fdae993c7df43257f2bb965ec70d37b37ea05164
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-30 11:54:16 -0300

    feat: implementacao da logica de servico e relatorios SQL

--- manutencao.nest/src/manutencao/manutencao.service.ts
+++ manutencao.nest/src/manutencao/manutencao.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { Manutencao } from './entities/manutencao.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateManutencaoDto } from './dto/create-manutencao.dto';
-import { UpdateManutencaoDto } from './dto/update-manutencao.dto';
 
 @Injectable()
 export class ManutencaoService {
+  constructor(
+    @InjectRepository(Manutencao)
+    private manutencaoRepository: Repository<Manutencao>,
+  ) {}
+
+  //Criar uma Nova Manutenção
   create(createManutencaoDto: CreateManutencaoDto) {
-    return 'This action adds a new manutencao';
+    const novaManutencao =
+      this.manutencaoRepository.create(createManutencaoDto);
+    return this.manutencaoRepository.save(novaManutencao);
   }
 
+  // Lista Simples (SELECT * FROM manutencao)
   findAll() {
-    return `This action returns all manutencao`;
+    return this.manutencaoRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} manutencao`;
+  // 1. Relatório Completo com JOINS
+  async gerarRelatorio() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return await this.manutencaoRepository.query(`
+      SELECT
+          m.codigo,
+          ts.nome as sistema,
+          tope.nome as operacao,
+          tc.nome as criticidade,
+          m.data_cadastro,
+          m.data_agendamento,
+          m.data_finalizada,
+          m.descricao
+      FROM manutencao m
+      JOIN tipo_sistema ts ON ts.codigo = m.codigo_tipo_sistema
+      JOIN tipo_operacao tope ON tope.codigo = m.codigo_tipo_operacao
+      JOIN tipo_criticidade tc ON tc.codigo = m.codigo_tipo_criticidade
+      ORDER BY m.data_cadastro DESC
+    `);
   }
-
-  update(id: number, updateManutencaoDto: UpdateManutencaoDto) {
-    return `This action updates a #${id} manutencao`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} manutencao`;
+  // 2. Dasbboard: Quantidade por sistema
+  async contarPorSistema() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return await this.manutencaoRepository.query(`
+      select
+      ts.nome,
+        count(*) as total
+      from manutencao m
+      join tipo_sistema ts on ts.codigo = m.codigo_tipo_sistema
+      group by ts.nome;
+    `);
   }
 }
