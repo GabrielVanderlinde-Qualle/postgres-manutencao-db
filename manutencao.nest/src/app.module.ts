66d6dba218c124a9157e5758393c23f5293be218
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-30 10:57:02 -0300

    feat: cria estrutura CRUD de manutencao

--- manutencao.nest/src/app.module.ts
+++ manutencao.nest/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ManutencaoModule } from './manutencao/manutencao.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: false, //Tabelas já Criadas no Banco Local
     }),
+    ManutencaoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
