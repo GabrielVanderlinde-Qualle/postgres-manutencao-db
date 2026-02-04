375a8b72ef4182549dbadf3a8c7a1469d4759356
GabrielVanderlinde-Qualle <gabriel.vanderlinde@quallecontrol.com.br>
2026-02-04 10:23:45 -0300

    feat(config): implementa documentacao Swagger e ajusta fuso horario para Sao_Paulo

--- manutencao.nest/src/main.ts
+++ manutencao.nest/src/main.ts
@@ -1,10 +1,20 @@
-process.env.TZ = 'America/Brasilia'; // Força o fuso horário
+process.env.TZ = 'America/Sao_Paulo';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Manutenção API')
+    .setDescription('API para Manutenções')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({
